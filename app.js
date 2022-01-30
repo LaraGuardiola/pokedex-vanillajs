097fc5cff02a046eabfb00f34bd1600a376aef61
LaraGuardiola <webdev.slara@gmail.com>
2022-01-30 22:06:06 +0100

    sonarqube analysis and code smells fixed

--- app.js
+++ app.js
@@ -6,55 +6,63 @@ function showPokemon(){
     fetch('https://pokeapi.co/api/v2/pokemon?limit=386') 
     .then(response => response.json())
     .then(function(data) {
-        for(let i = 0; i < data.results.length; i++){
-            let p = document.createElement('p')
-            midScreen.appendChild(p)
-            p.innerHTML = `${i+1}.${data.results[i].name}`
-            p.onclick = function(){
-                let p = getParagraphs()
-                p.forEach(p => p.remove())
-                let index = i + 1
-                showSprites(index,data.results[i].name)
-            }
-        }
+        getPokemonList(data)
     })
 }
 
-function showSprites(index,description){
+function getPokemonList(data){
+    for(let i = 0; i < data.results.length; i++){
+        let p = document.createElement('p')
+        midScreen.appendChild(p)
+        p.innerHTML = `${i+1}.${data.results[i].name}`
+        p.onclick = function(){
+            let pokemon = getParagraphs()
+            pokemon.forEach(poke => poke.remove())
+            let index = i + 1
+            showStats(index,data.results[i].name)
+        }
+    }
+}
+
+function showStats(index,description){
     fetch(`https://pokeapi.co/api/v2/pokemon-form/${index}`)
     .then(response => response.json())
     .then(src => {
-        let img = document.createElement('img')
-        midScreen.appendChild(img)
-        img.src = src.sprites.front_default
-        let backBtn = document.createElement('p')
-        midScreen.appendChild(backBtn)
-        setTimeout(()=>{
-            backBtn.setAttribute('class', "back-btn")
-            backBtn.innerHTML = "back"
-        },100)
-        createStats(midScreen,"stat1","name",description)
-        createStats(midScreen,"stat2","type",src.types[0].type.name)
         getHabitat(src.id)
-        backBtn.onclick = function(){
-            let sons = removeSprite()
-            sons.forEach(son =>{
-                son.remove()
-            })
-            showPokemon()
-        }
+        printSprite(src)
+        createStats("stat1","name",description)
+        createStats("stat2","type",src.types[0].type.name) 
     })
+}
+
+function printSprite(src){
+    let img = document.createElement('img')
+    midScreen.appendChild(img)
+    img.src = src.sprites.front_default
+    let backBtn = document.createElement('p')
+    midScreen.appendChild(backBtn)
+    setTimeout(()=>{
+        backBtn.setAttribute('class', "back-btn")
+        backBtn.innerHTML = "back"
+    },100)
+    backBtn.onclick = function(){
+        let sons = removeSprite()
+        sons.forEach(son =>{
+            son.remove()
+        })
+        showPokemon()
+    }
 }
 
 function getHabitat(id){
     fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
     .then(response => response.json())
     .then(data => {
-        createStats(midScreen,"stat3","habitat", data.habitat.name)
+        createStats("stat3","habitat", data.habitat.name)
     })
 }
 
-function createStats(midScreen,className,stat,description){
+function createStats(className,stat,description){
     let pokemonName = document.createElement('p')
     midScreen.appendChild(pokemonName)
     pokemonName.innerHTML = `${stat}: ${description}`
@@ -62,11 +70,9 @@ function createStats(midScreen,className,stat,description){
 }
 
 function removeSprite(){
-    let sons = document.querySelectorAll('.mid-screen > *')
-    return sons
+    return document.querySelectorAll('.mid-screen > *')
 }
 
 function getParagraphs(){
-    let p = document.querySelectorAll('.mid-screen > p')
-    return p
+    return document.querySelectorAll('.mid-screen > p')
 }
